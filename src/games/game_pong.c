44368fc428ad32ba5bf0bec83a488e507b8bbb88
rfmineguy <rfmineguy@gmail.com>
2022-10-07 21:47:14 -0700

    Began the first game to be made with FFLib, Pong

--- src/games/game_pong.c
+++ src/games/game_pong.c
@@ -2,6 +2,7 @@
 #include <firefly/IO/IO.h>
 #include <firefly/Resource/Resource.h>
 #include <firefly/Rendering/Renderer.h>
+#include <firefly/Core/OrthoCamera.h>
 
 void setup_keybinds() {
   FF_KeyBindRegister("left", KEY_NONE, KEY_LEFT);
@@ -31,6 +32,38 @@ int main() {
   init();
   Window* w = FF_CreateWindowGL("Pong", 600, 600);
   FF_InitRenderer();
+  
+  Geometry quad = GenerateQuad();
+  Camera c = FF_OrthoCamera();
+  Texture t = FF_LoadTexture("res/splotch.png");
+  
+  int left_y = 0;
+  int right_y = 0;
+  
+  while (!FF_WindowShouldClose(w)) {
+    FF_WindowPollEvents(w);
+    FF_WindowClearBackgroundEx(w, 0.4f, 0.4f, 0.4f, 1.0f);
+    
+    FF_OrthoCameraUpdate(&c, false);
+    
+    if (FF_IsKeyDown(KEY_S))
+      left_y--;
+    if (FF_IsKeyDown(KEY_W))
+      left_y++;
+    if (FF_IsKeyDown(KEY_DOWN))
+      right_y--;
+    if (FF_IsKeyDown(KEY_UP))
+      right_y++;
+    
+    // draw left paddle
+    FF_BindTexture(t);
+    FF_RendererDrawGeometryEx(quad, c, (vec3){-7, left_y, 0}, (vec3){1, 8, 1}, (vec3){0, 0, 0}, 0);
+    FF_RendererDrawGeometryEx(quad, c, (vec3){7, right_y, 0}, (vec3){1, 8, 1}, (vec3){0, 0, 0}, 0);
+    // draw right paddle
+  }
+  
+  FreeGeometry(quad);
+  FF_FreeTexture(t);
   
   deinit(w);
 }
