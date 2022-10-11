f94b1b4cfc6572b13af31c00ba2bb40a83370d3a
rfmineguy <rfmineguy@gmail.com>
2022-10-10 21:28:11 -0700

    A commit marking the point of having a working version of the Pong game using FireflyLib

--- src/games/game_pong.c
+++ src/games/game_pong.c
@@ -3,24 +3,19 @@
 #include <firefly/Resource/Resource.h>
 #include <firefly/Rendering/Renderer.h>
 #include <firefly/Core/OrthoCamera.h>
+#include <firefly/Core/PerspectiveCamera.h>
+#include <stdbool.h>
 
 typedef struct {
   float x, y, w, h;
 } Rect;
 
 typedef struct {
-  vec3 pos;
-  vec3 scale;
-  vec3 rot_axis;
-  float rotation;
-} Transform;
+  Rect r;
+  float vel_x, vel_y;
+} Ball;
 
-void setup_keybinds() {
-  FF_KeyBindRegister("left", KEY_NONE, KEY_LEFT);
-  FF_KeyBindRegister("right", KEY_NONE, KEY_RIGHT);
-  FF_KeyBindRegister("up", KEY_NONE, KEY_UP);
-  FF_KeyBindRegister("down", KEY_NONE, KEY_DOWN);
-}
+void setup_keybinds() {}
 
 void init() {
   SetLogStream(stdout);
@@ -39,6 +34,44 @@ void deinit(Window* w) {
   FF_DestroyWindowGL(w);
 }
 
+void run_left_paddle(Rect* left_paddle, int h) {
+    if (FF_IsKeyDown(KEY_S))
+      left_paddle->y += 4;
+    if (FF_IsKeyDown(KEY_W))
+      left_paddle->y -= 4;
+    if (left_paddle->y < 0)
+      left_paddle->y = 0;
+    if (left_paddle->y > h)
+      left_paddle->y = h;
+}
+void run_right_paddle(Rect* right_paddle, int h) {
+    if (FF_IsKeyDown(KEY_DOWN))
+      right_paddle->y += 4;
+    if (FF_IsKeyDown(KEY_UP))
+      right_paddle->y -= 4;
+    if (right_paddle->y < 0)
+      right_paddle->y = 0;
+    if (right_paddle->y > h)
+      right_paddle->y = h;
+}
+void run_ball(Ball* ball, Window* w) {
+  if (ball->r.x < 0 || ball->r.x > FF_WindowGetWidth(w)) {
+    ball->vel_x *= -1;
+  }
+  if (ball->r.y < 0 || ball->r.y > FF_WindowGetHeight(w)) {
+    ball->vel_y *= -1;
+  }
+  ball->r.x += ball->vel_x * FF_WindowDeltaTime(w);
+  ball->r.y += ball->vel_y * FF_WindowDeltaTime(w);
+}
+
+//collision assuming x,y is the center
+bool collided(Rect* a, Rect* b) {
+  bool xOverlap = a->x < b->x + b->w && a->x + a->w > b->x;
+  bool yOveralp = a->y < b->y + b->h && a->y + a->h > b->y;
+  return xOverlap && yOveralp;
+}
+
 int main() {
   init();
   Window* w = FF_CreateWindowGL("Pong", 600, 600);
@@ -47,24 +80,42 @@ int main() {
   Geometry quad = GenerateQuad();
   Camera c = FF_OrthoCamera();
   Texture t = FF_LoadTexture("res/splotch.png");
-  //Material m = FF_Material();
-  //m.albedo = (Color){255, 100, 150};
-  
-  int ball_x = 300, ball_y = 300;
+  Ball ball = (Ball) {.r=(Rect){300, 300, 60, 20}, .vel_x=100, .vel_y=150 };
+  Rect left_paddle = (Rect){40, FF_WindowGetHeight(w) / 2.f - 75, 10, 150};
+  Rect right_paddle = (Rect){FF_WindowGetWidth(w) - 50, FF_WindowGetHeight(w) / 2.f - 75, 10, 150};
+  int left_score = 0, right_score = 0;
   
   while (!FF_WindowShouldClose(w)) {
     FF_WindowPollEvents(w);
     FF_WindowClearBackgroundEx(w, 0.4f, 0.4f, 0.4f, 1.0f);
     if (FF_WasWindowResized(w)) {
+      LOG_DEBUG("WindowWidth: %d, WindowHeight: %d", FF_WindowGetWidth(w), FF_WindowGetHeight(w));
       FF_OrthoCameraUpdateProj(&c, FF_WindowGetWidth(w), FF_WindowGetHeight(w));
     }
     
-    FF_OrthoCameraUpdate(&c, true);
-    
+    FF_OrthoCameraUpdate(&c, false);
     FF_BindTexture(t);
     
+    run_left_paddle(&left_paddle, FF_WindowGetHeight(w));
+    run_right_paddle(&right_paddle, FF_WindowGetHeight(w));
+    run_ball(&ball, w);
+
+    if (collided(&ball.r, &left_paddle)) {
+      LOG_INFO("Collision");
+    }
+    if (collided(&ball.r, &left_paddle) || collided(&ball.r, &right_paddle)) {
+      ball.vel_x *= -1;
+    }
+    
+    if (FF_IsKeyPressed(KEY_R)) {
+      ball.r.x = FF_WindowGetWidth(w) / 2.f;
+      ball.r.y = FF_WindowGetHeight(w) / 2.f;
+    }
+
     // draw ball
-    FF_RendererDrawGeometryEx(quad, c, (vec3){5, 7, 0}, (vec3){2, 1, 1}, (vec3){0, 0, 0}, 0);
+    FF_RendererDrawGeometryEx(quad, c, (vec3){ball.r.x, ball.r.y, 0}, (vec3){ball.r.w, ball.r.h, 1}, (vec3){0}, 0);
+    FF_RendererDrawGeometryEx(quad, c, (vec3){left_paddle.x, left_paddle.y, 0}, (vec3){left_paddle.w, left_paddle.h, 1}, (vec3){0}, 0);
+    FF_RendererDrawGeometryEx(quad, c, (vec3){right_paddle.x, right_paddle.y, 0}, (vec3){right_paddle.w, right_paddle.h, 1}, (vec3){0}, 0);
   }
   
   FreeGeometry(quad);
